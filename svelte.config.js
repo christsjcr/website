4f704e50ff0c5188d7f73d874b907b294e9324cd
Miles Watson <contact@mileswatson.net>
2021-12-15 16:25:11 +0000

    Used svelte-add to create a Bulma+SCSS template.

---
+++ svelte.config.js
@@ -0,0 +1,34 @@
+import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      scss: {
+        prependData: '@import "src/variables.scss";',
+      },
+    }),
+  ],
+
+  kit: {
+    adapter: adapter(),
+
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+
+    vite: {
+      css: {
+        preprocessorOptions: {
+          scss: {
+            additionalData: '@import "src/variables.scss";',
+          },
+        },
+      },
+    },
+  },
+};
+
+export default config;
