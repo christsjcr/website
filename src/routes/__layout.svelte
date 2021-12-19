bc71ad16bf7520cc19046596fbc2671a358fe819
Miles Watson <contact@mileswatson.net>
2021-12-19 13:20:47 +0000

    Disabled animations when resizing.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+
     import "../app.scss";
     import type { NavbarItem } from "../components/Navbar.svelte";
     import Navbar from "../components/Navbar.svelte";
@@ -16,8 +18,32 @@
         { route: "/roles", label: "Roles" },
         { route: "/meetings", label: "Meetings" },
     ];
+
+    // Adds a listener to disable animations on resizing.
+    // Initially tried in pure Svelte, but couldn't get it to work.
+    onMount(() => {
+        const classes = document.body.classList;
+        let timer;
+        window.addEventListener("resize", function () {
+            if (timer) {
+                clearTimeout(timer);
+                timer = null;
+            } else classes.add("stop-transitions");
+
+            timer = setTimeout(() => {
+                classes.remove("stop-transitions");
+                timer = null;
+            }, 100);
+        });
+    });
 </script>
 
 <Navbar {layout}>
     <slot />
 </Navbar>
+
+<style>
+    :global(body.stop-transitions *) {
+        transition: none !important;
+    }
+</style>
