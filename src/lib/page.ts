94821d89d3fb0e30a721405a6a6d44d3e2f47c05
Miles Watson <contact@mileswatson.net>
2021-12-21 23:18:32 +0000

    Enforced transparent limit on page interface.

--- src/lib/page.ts
+++ src/lib/page.ts
@@ -2,8 +2,8 @@ import { writable, type Writable } from "svelte/store";
 
 export interface Page {
     current: string;
-    transparentLimit?: HTMLElement;
+    transparentLimit: HTMLElement;
     hideLogo?: boolean;
 }
 
-export const page: Writable<Page> = writable({ current: null });
+export const page: Writable<Page> = writable({ current: null, transparentLimit: null });
