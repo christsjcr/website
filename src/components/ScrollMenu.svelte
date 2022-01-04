dc8b6738e08689e717526f94bc72fea0ddadc562
Miles Watson <contact@mileswatson.net>
2022-01-04 22:24:39 +0000

    Created ScrollMenu component.

---
+++ src/components/ScrollMenu.svelte
@@ -0,0 +1,84 @@
+<script context="module" lang="ts">
+    export interface Title {
+        label?: string;
+        title: string;
+        id: string;
+        [other: string]: any;
+    }
+
+    export interface Category {
+        category: string;
+        roles: Title[];
+        [other: string]: any;
+    }
+</script>
+
+<script lang="ts">
+    export let categories: Category[];
+
+    function scrollTo(e: Event, id: string) {
+        e.preventDefault();
+        let pos = document.getElementById(id).getBoundingClientRect().top;
+        let doc = document.documentElement;
+        let top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+        animateScrollTo(top + pos - 70, 500);
+    }
+
+    const animateScrollTo = (to: number, duration: number) => {
+        const element = document.scrollingElement || document.documentElement;
+        const start = element.scrollTop;
+        const change = to - start;
+        const startDate = +new Date();
+        // t = current time
+        // b = start value
+        // c = change in value
+        // d = duration
+        const easeInOutQuad = (t, b, c, d) => {
+            let t2 = t;
+            t2 /= d / 2;
+            if (t2 < 1) return (c / 2) * t2 * t2 + b;
+            t2 -= 1;
+            return (-c / 2) * (t2 * (t2 - 2) - 1) + b;
+        };
+        const animateScroll = () => {
+            const currentDate = +new Date();
+            const currentTime = currentDate - startDate;
+            element.scrollTop = parseInt(
+                easeInOutQuad(currentTime, start, change, duration),
+                10
+            );
+            if (currentTime < duration) {
+                requestAnimationFrame(animateScroll);
+            } else {
+                element.scrollTop = to;
+            }
+        };
+        animateScroll();
+    };
+</script>
+
+<aside class="menu my-6">
+    <ul class="menu-list">
+        {#each categories as category}
+            <li>
+                <a
+                    class="is-active"
+                    href={`#${category.category}`}
+                    on:click={(e) => scrollTo(e, category.category)}
+                    >{category.category}</a
+                >
+                <ul>
+                    {#each category.roles as role}
+                        <li>
+                            <a
+                                href={`#${role.id}`}
+                                on:click={(e) => scrollTo(e, role.id)}
+                                >{role.label ? role.label : role.title}
+                            </a>
+                        </li>
+                    {/each}
+                </ul>
+            </li>
+        {/each}
+    </ul>
+</aside>
--- src/routes/about/roles.svelte
+++ src/routes/about/roles.svelte
@@ -1,64 +1,12 @@
 <script lang="ts">
     import PageHeader from "@components/PageHeader.svelte";
+    import ScrollMenu from "@components/ScrollMenu.svelte";
+    import type { Category } from "@components/ScrollMenu.svelte";
 
-    import width, { desktop, widescreen } from "@lib/width";
+    import width, { desktop } from "@lib/width";
 
     $: profileColumns = $width >= 512;
     $: fromDesktop = $width >= desktop.min;
-    $: fromWidescreen = $width >= widescreen.min;
-
-    interface Role {
-        label?: string;
-        title: string;
-        name: string;
-        email_prefix: string;
-        img?: string;
-    }
-
-    interface Category {
-        category: string;
-        roles: Role[];
-    }
-
-    function scrollTo(e: Event, id: string) {
-        e.preventDefault();
-        let pos = document.getElementById(id).getBoundingClientRect().top;
-        let doc = document.documentElement;
-        let top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
-        animateScrollTo(top + pos - 70, 500);
-    }
-
-    const animateScrollTo = (to: number, duration: number) => {
-        const element = document.scrollingElement || document.documentElement;
-        const start = element.scrollTop;
-        const change = to - start;
-        const startDate = +new Date();
-        // t = current time
-        // b = start value
-        // c = change in value
-        // d = duration
-        const easeInOutQuad = (t, b, c, d) => {
-            let t2 = t;
-            t2 /= d / 2;
-            if (t2 < 1) return (c / 2) * t2 * t2 + b;
-            t2 -= 1;
-            return (-c / 2) * (t2 * (t2 - 2) - 1) + b;
-        };
-        const animateScroll = () => {
-            const currentDate = +new Date();
-            const currentTime = currentDate - startDate;
-            element.scrollTop = parseInt(
-                easeInOutQuad(currentTime, start, change, duration),
-                10
-            );
-            if (currentTime < duration) {
-                requestAnimationFrame(animateScroll);
-            } else {
-                element.scrollTop = to;
-            }
-        };
-        animateScroll();
-    };
 
     const categories: Category[] = [
         {
@@ -67,22 +15,22 @@
                 {
                     title: "President",
                     name: "Sam Carling",
-                    email_prefix: "president",
+                    id: "president",
                 },
                 {
                     title: "Vice President",
                     name: "Tamsin Wood",
-                    email_prefix: "vicepresident",
+                    id: "vicepresident",
                 },
                 {
                     title: "Treasurer",
                     name: "Fergus Kirman",
-                    email_prefix: "treasurer",
+                    id: "treasurer",
                 },
                 {
                     title: "Secretary",
                     name: "Immi Sandhu",
-                    email_prefix: "secretary",
+                    id: "secretary",
                 },
             ],
         },
@@ -93,25 +41,25 @@
                     title: "Freshers' Reps",
                     label: "Freshers'",
                     name: "Vivek Bilous & Charlotte Westwood",
-                    email_prefix: "firstyearrep",
+                    id: "firstyearrep",
                 },
                 {
                     title: "Internationals' Rep",
                     label: "Internationals'",
                     name: "Tudor Serban",
-                    email_prefix: "intl",
+                    id: "intl",
                 },
                 {
                     title: "Events Officer (Ents)",
                     label: "Ents",
                     name: "Hannah Huang",
-                    email_prefix: "ents",
+                    id: "ents",
                 },
                 {
                     title: "Catering & Facilities Officer",
                     label: "Catering & Facilities",
                     name: "Jonathon Konig",
-                    email_prefix: "facilities",
+                    id: "facilities",
                 },
             ],
         },
@@ -122,13 +70,13 @@
                     title: "Male & NB Welfare Officer",
                     label: "Male & NB",
                     name: "Ossie Visick",
-                    email_prefix: "welfare-m",
+                    id: "welfare-m",
                 },
                 {
                     title: "Female & NB Welfare Officer",
                     label: "Female & NB",
                     name: "Christina Lawrence",
-                    email_prefix: "welfare-f",
+                    id: "welfare-f",
                 },
             ],
         },
@@ -139,25 +87,25 @@
                     title: "Access Officer",
                     label: "Access",
                     name: "Romany Whittall",
-                    email_prefix: "access",
+                    id: "access",
                 },
                 {
                     title: "Ethnic Diversity Officer",
                     label: "Ethnic Diversity",
                     name: "Abhi Gowda",
-                    email_prefix: "edo",
+                    id: "edo",
                 },
                 {
                     title: "LGBT+ Officer",
                     label: "LGBT+",
                     name: "Kit Burley",
-                    email_prefix: "lgbt+",
+                    id: "lgbt+",
                 },
                 {
                     title: "Women's Officer",
                     label: "Women's",
                     name: "Leah Almeida",
-                    email_prefix: "womens",
+                    id: "womens",
                 },
             ],
         },
@@ -168,12 +116,12 @@
                     title: "Green Officer",
                     label: "Green",
                     name: "Camille McCarthy",
-                    email_prefix: "green",
+                    id: "green",
                 },
                 {
                     title: "Webmaster",
                     name: "Miles Watson",
-                    email_prefix: "webmaster",
+                    id: "webmaster",
                 },
             ],
         },
@@ -195,34 +143,7 @@
     </div>
     <div class="columns is-variable is-5 is-desktop">
         <div class="column is-one-third-desktop is-one-quarter-widescreen">
-            <aside class="menu my-6">
-                <ul class="menu-list">
-                    {#each categories as category}
-                        <li>
-                            <a
-                                class="is-active"
-                                href={`#${category.category}`}
-                                on:click={(e) => scrollTo(e, category.category)}
-                                >{category.category}</a
-                            >
-                            <ul>
-                                {#each category.roles as role}
-                                    <li>
-                                        <a
-                                            href={`#${role.email_prefix}`}
-                                            on:click={(e) =>
-                                                scrollTo(e, role.email_prefix)}
-                                            >{role.label
-                                                ? role.label
-                                                : role.title}
-                                        </a>
-                                    </li>
-                                {/each}
-                            </ul>
-                        </li>
-                    {/each}
-                </ul>
-            </aside>
+            <ScrollMenu {categories} />
         </div>
         <div class="column">
             {#each categories as category}
@@ -232,7 +153,7 @@
                     </h2>
                     {#each category.roles as role}
                         <div
-                            id={role.email_prefix}
+                            id={role.id}
                             class="columns"
                             class:is-mobile={profileColumns}
                         >
@@ -256,10 +177,10 @@
                                         </div>
 
                                         <a
-                                            href={`mailto:${role.email_prefix}@thejcr.co.uk`}
+                                            href={`mailto:${role.id}@thejcr.co.uk`}
                                             ><small
                                                 class:level-right={fromDesktop}
-                                                >{role.email_prefix}@thejcr.co.uk</small
+                                                >{role.id}@thejcr.co.uk</small
                                             ></a
                                         >
                                     </div>
