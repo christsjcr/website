68af09eeff5cd8993a20dbd6ee499e08e9062b26
Miles Watson <contact@mileswatson.net>
2022-01-21 20:53:50 +0000

    Added meetings content.

--- src/routes/meetings.svelte
+++ src/routes/meetings.svelte
@@ -2,4 +2,47 @@
     import PageHeader from "@components/PageHeader.svelte";
 </script>
 
-<PageHeader current="/meetings" title="Meetings" />
+<PageHeader current="/meetings" title="Meetings">
+    <div class="content">
+        <h1>Open Meetings</h1>
+        <p>
+            Undergraduate students of Christ's may attend an open meeting with
+            the right to vote on all motions or amendments proposed during the
+            meeting.
+        </p>
+        <p>Open meetings typically cover topics such as:</p>
+        <ul>
+            <li>
+                the making of new societies (after the original Michaelmas
+                deadline)
+            </li>
+            <li>current issues raised by students</li>
+            <li>changes to the JCR Constitution</li>
+            <li>voting in SU elections</li>
+            <li>the signing of open letters</li>
+        </ul>
+        <h1>JCR Meetings</h1>
+        <p>
+            Undergraduate students of Christ's may attend JCR meetings, although
+            they do not have the right to vote.
+        </p>
+        <h3>Council Meetings</h3>
+        <p>
+            All council members are expected to attend these meetings. Any
+            member of the JCR may attend and can speak with permission of the
+            Chair, though may not vote.
+        </p>
+        <p>
+            Business declared reserved by the Chair may only be heard by members
+            of the Council, and the Chair shall have the power to ask other
+            attendees to leave when such business is raised.
+        </p>
+
+        <h3>Exec Meetings</h3>
+        <p>
+            These are similar to Exec meetings, except only Exec members (the
+            President, Vice President, Treasurer, Welfare Officers and
+            Secretary) are expected to attend.
+        </p>
+    </div>
+</PageHeader>
