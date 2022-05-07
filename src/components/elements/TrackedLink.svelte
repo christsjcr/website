9a0538dc964b48e20d9783c4c319aadac05acbdb
Miles Watson <contact@mileswatson.net>
2022-05-07 20:54:21 +0100

    Added tracked link component.

---
+++ src/components/elements/TrackedLink.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let href: string;
+    export let id: string;
+    export let external: boolean = false;
+
+    import ClickTracker from "./ClickTracker.svelte";
+</script>
+
+<ClickTracker {id}>
+    <a {href} target={external ? "_blank" : ""}>
+        <slot />
+    </a>
+</ClickTracker>
