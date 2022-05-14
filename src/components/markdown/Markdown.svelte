0de907637ecd6e40f4e266f5637637629035f993
Miles Watson <contact@mileswatson.net>
2022-05-14 17:55:40 +0100

    Fixed external links in resources.

---
+++ src/components/markdown/Markdown.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import ExternalLink from "./ExternalLink.svelte";
+  import SvelteMarkdown, { type Renderers } from "svelte-markdown";
+
+  export let source: string;
+  export let externalLinks: boolean = false;
+
+  const renderers: Partial<Renderers> = {};
+
+  if (externalLinks) renderers.link = ExternalLink;
+</script>
+
+<SvelteMarkdown {source} {renderers} />
