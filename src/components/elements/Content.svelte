9c1edd04ebc8a9e2006373fb2905714a069916f5
Miles Watson <contact@mileswatson.net>
2022-09-26 22:52:57 +0100

    Narrowed freshers timetable.

--- src/components/elements/Content.svelte
+++ src/components/elements/Content.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     export let centered = true;
+    export let hasText = true;
 </script>
 
 <div class="columns" class:is-centered={centered}>
     <div class="column is-narrow">
-        <div class="content text">
+        <div class="${hasText ? 'content ' : ''}text">
             <slot />
         </div>
     </div>
