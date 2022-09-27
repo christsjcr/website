26f4fd7faa5e345a8cff3e173d96574b8ed5b44a
Miles Watson <contact@mileswatson.net>
2022-09-27 12:37:39 +0100

    Reworked home page layout.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,9 @@
     import NotificationLink from "$components/elements/NotificationLink.svelte";
     import PageHeader from "$components/PageHeader.svelte";
     import week from "$lib/week";
-
-    import Mode from "$components/Mode.svelte";
-    import type { Writable } from "svelte/store";
     import UpperHallMenu from "$components/features/UpperHallMenu.svelte";
     import TrackedLink from "$components/elements/TrackedLink.svelte";
-
-    let mode: Writable<"current_student" | "visitor">;
+    import width, { desktop } from "$lib/width";
 </script>
 
 <PageHeader
@@ -28,144 +24,134 @@
         Cambridge. We are your student-elected Committee, representing your
         interests to help you make the most of your time here!
     </div>
-    {#if $mode === "visitor"}
-        <div class="columns my-6">
-            <div class="column is-one-third">
-                <NotificationLink
-                    href="/about/committee"
-                    title="The Committee 🐐"
-                    content="Get to know the current members of the JCR Committee!"
-                />
-            </div>
-            <div class="column is-one-third">
-                <NotificationLink
-                    href="/get-involved"
-                    title="Get Involved 💃"
-                    content="Find out what's going on at Christ's, and explore the complete list of Christ's societies!"
-                />
-            </div>
-            <div class="column is-one-third">
-                <NotificationLink
-                    href="/resources/welfare"
-                    title="Welfare Resources 💛"
-                    content="Resources for general and targeted support - anything related to physical, sexual, or mental health."
-                />
-            </div>
+
+    <div class="columns my-6">
+        <div class="column is-one-third">
+            <NotificationLink
+                href="/about/committee"
+                title="The Committee 🐐"
+                content="Get to know the current members of the JCR Committee!"
+            />
         </div>
-    {:else}
-        <h2 class="title is-2">Updates & Feedback</h2>
-        <div class="columns m-1 is-desktop">
-            <!--
-            <div class="column is-two-thirds-desktop">
-                <TitleBox header="Updates ⚙️">
-                    <div class="content">
-                        <p>Nothing to see here at the moment!</p>
-                    </div>
-                </TitleBox>
-            </div>
-            -->
-            <div class="column is-half is-one-third-desktop">
+        <div class="column is-one-third">
+            <NotificationLink
+                href="/get-involved"
+                title="Get Involved 💃"
+                content="Find out what's going on at Christ's, and explore the complete list of Christ's societies!"
+            />
+        </div>
+        <div class="column is-one-third">
+            <NotificationLink
+                href="/resources/welfare"
+                title="Welfare Resources 💛"
+                content="Resources for general and targeted support - anything related to physical, sexual, or mental health."
+            />
+        </div>
+    </div>
+    <div class="columns my-6 is-desktop">
+        <div class="column is-two-thirds-desktop">
+            <UpperHallMenu />
+        </div>
+        <div class="column is-one-third{$width > desktop.min ? ' pt-4' : ''}">
+            {#if $width > desktop.min}
+                <br />
+                <br />
+            {/if}
+            <br />
+            <div>
                 <NotificationLink
                     href="https://docs.google.com/forms/d/e/1FAIpQLSchG-F00FNgO02T5OepdfuFSkVi7HgnfpI0a6qDjhhXctG6Hw/viewform"
                     title="Feedback Form ✍️"
-                    content="If you want to provide feedback, suggestions, or general problems for the JCR to consider, please use this form!
-                        The JCR is there to work for you, so any and all feedback is welcome."
+                    content="If you want to provide feedback, suggestions, or general problems for the JCR to consider, please use this form! The JCR is there to work for you, so any and all feedback is welcome."
                     external
                     grey
                 />
             </div>
         </div>
-        <br />
-        <h2 class="title is-2">Upper Hall Menu</h2>
-        <UpperHallMenu />
-        <br />
-        <h2 class="title is-2">Quick Links</h2>
-        <div class="columns m-1 clickable-lists">
-            <div class="column">
-                <div class="content">
-                    <subtitle class="subtitle">Food</subtitle>
-                    <ul class="list">
-                        <li>
-                            <TrackedLink
-                                href={`https://intranet.christs.cam.ac.uk/upper-hall-menus#Week${week()}`}
-                                id="intranet-menu"
-                                external>Menu</TrackedLink
-                            >
-                        </li>
-                        <li>
-                            <TrackedLink
-                                href="https://intranet.christs.cam.ac.uk/Shibboleth.sso/Login?target=%2Fshibboleth%2Flogin%3Fshiblogin%3D1%26destination%3D%2Fmealbooking%2F"
-                                id="meal-booking">Meal booking</TrackedLink
-                            >
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="column">
-                <div class="content">
-                    <subtitle class="subtitle">Facilities</subtitle>
-                    <ul class="list">
-                        <li>
-                            <TrackedLink
-                                href="https://christscollege.roombookingsystem.co.uk/overviewday"
-                                id="room-booking"
-                                external>Room booking</TrackedLink
-                            >
-                        </li>
-                        <li>
-                            <TrackedLink
-                                href="https://intranet.christs.cam.ac.uk/fault-reporting"
-                                id="fault-reporting"
-                                external>Fault reporting</TrackedLink
-                            >
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="column">
-                <div class="content">
-                    <subtitle class="subtitle">Porters</subtitle>
-                    <ul class="list">
-                        <li>
-                            <TrackedLink
-                                href="tel:+44-1223-334900"
-                                id="phone-porters">+44 1223 334900</TrackedLink
-                            >
-                        </li>
-                        <li>
-                            <TrackedLink
-                                href="mailto:porters@christs.cam.ac.uk"
-                                id="email-porters"
-                                >porters@christs.cam.ac.uk</TrackedLink
-                            >
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="column">
-                <div class="content">
-                    <subtitle class="subtitle">Finance</subtitle>
-                    <ul class="list">
-                        <li>
-                            <TrackedLink
-                                href="https://docs.google.com/forms/d/e/1FAIpQLSdjETgdH68GhpuQMi8jonTSEwZYkElj1VkjHfTC13kJCPmBFw/viewform"
-                                id="reimbursement-form"
-                                external>Reimbursement</TrackedLink
-                            >
-                        </li>
-                        <li>
-                            <TrackedLink
-                                href="https://docs.google.com/forms/d/e/1FAIpQLSf1Hz_a6Ceg49VoDovKoQNxaTmPi6pRGYHd3nVtbtSjcxqkAw/viewform"
-                                id="prescription-form"
-                                external>Prescription Scheme</TrackedLink
-                            >
-                        </li>
-                    </ul>
-                </div>
+    </div>
+    <h2 class="title is-2">Quick Links</h2>
+    <div class="columns m-1 clickable-lists">
+        <div class="column">
+            <div class="content">
+                <subtitle class="subtitle">Food</subtitle>
+                <ul class="list">
+                    <li>
+                        <TrackedLink
+                            href={`https://intranet.christs.cam.ac.uk/upper-hall-menus#Week${week()}`}
+                            id="intranet-menu"
+                            external>Menu</TrackedLink
+                        >
+                    </li>
+                    <li>
+                        <TrackedLink
+                            href="https://intranet.christs.cam.ac.uk/Shibboleth.sso/Login?target=%2Fshibboleth%2Flogin%3Fshiblogin%3D1%26destination%3D%2Fmealbooking%2F"
+                            id="meal-booking">Meal booking</TrackedLink
+                        >
+                    </li>
+                </ul>
             </div>
         </div>
-    {/if}
-    <div class="has-text-centered mt-6">
-        <Mode bind:mode />
+        <div class="column">
+            <div class="content">
+                <subtitle class="subtitle">Facilities</subtitle>
+                <ul class="list">
+                    <li>
+                        <TrackedLink
+                            href="https://christscollege.roombookingsystem.co.uk/overviewday"
+                            id="room-booking"
+                            external>Room booking</TrackedLink
+                        >
+                    </li>
+                    <li>
+                        <TrackedLink
+                            href="https://intranet.christs.cam.ac.uk/fault-reporting"
+                            id="fault-reporting"
+                            external>Fault reporting</TrackedLink
+                        >
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="column">
+            <div class="content">
+                <subtitle class="subtitle">Porters</subtitle>
+                <ul class="list">
+                    <li>
+                        <TrackedLink
+                            href="tel:+44-1223-334900"
+                            id="phone-porters">+44 1223 334900</TrackedLink
+                        >
+                    </li>
+                    <li>
+                        <TrackedLink
+                            href="mailto:porters@christs.cam.ac.uk"
+                            id="email-porters"
+                            >porters@christs.cam.ac.uk</TrackedLink
+                        >
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="column">
+            <div class="content">
+                <subtitle class="subtitle">Finance</subtitle>
+                <ul class="list">
+                    <li>
+                        <TrackedLink
+                            href="https://docs.google.com/forms/d/e/1FAIpQLSdjETgdH68GhpuQMi8jonTSEwZYkElj1VkjHfTC13kJCPmBFw/viewform"
+                            id="reimbursement-form"
+                            external>Reimbursement</TrackedLink
+                        >
+                    </li>
+                    <li>
+                        <TrackedLink
+                            href="https://docs.google.com/forms/d/e/1FAIpQLSf1Hz_a6Ceg49VoDovKoQNxaTmPi6pRGYHd3nVtbtSjcxqkAw/viewform"
+                            id="prescription-form"
+                            external>Prescription Scheme</TrackedLink
+                        >
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </PageHeader>
