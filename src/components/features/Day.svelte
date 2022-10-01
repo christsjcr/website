87387d3520ee8baadea8895cfc2ef2ef1a8cd90b
Miles Watson <contact@mileswatson.net>
2022-10-01 15:51:29 +0100

    Updated menu message.

--- src/components/features/Day.svelte
+++ src/components/features/Day.svelte
@@ -53,7 +53,7 @@
                     <p>Menu Unavailable!</p>
                 </div>
                 <div class="message-body">
-                    Menu data is only available during term times.
+                    Menu will be available when college update the intranet (hopefully Tuesday or Wednesday).
                 </div>
             </article>
         {/if}
