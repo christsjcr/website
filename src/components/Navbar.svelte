8ec34fd94225ae5cfbda1b15bdba420feb52d0d7
Miles Watson <contact@mileswatson.net>
2022-10-14 16:10:28 +0100

    Formatting.

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -54,20 +54,17 @@
             class={"navbar is-fixed-top is-transparent"}
             class:barshow={notTransparent}
             class:pr-4={fromDesktop}
-            class:animated
-        >
+            class:animated>
             <div class="navbar-brand">
                 <a
                     class={"navbar-item logo"}
                     class:logoshow={$page.type != "image" || notTransparent}
-                    href="/"
-                >
+                    href="/">
                     <img
                         src="/img/logo_small.webp"
                         width="112"
                         height="28"
-                        alt="JCR Logo"
-                    />
+                        alt="JCR Logo" />
                 </a>
                 <p
                     role="button"
@@ -75,8 +72,7 @@
                     class:is-active={expanded}
                     aria-label="menu"
                     aria-expanded={expanded}
-                    on:click={() => (expanded = !expanded)}
-                >
+                    on:click={() => (expanded = !expanded)}>
                     <span style="color:white" aria-hidden="true" />
                     <span style="color:white" aria-hidden="true" />
                     <span style="color:white" aria-hidden="true" />
@@ -85,8 +81,7 @@
             <div
                 class="navbar-menu"
                 class:is-active={expanded}
-                on:click={() => (expanded = false)}
-            >
+                on:click={() => (expanded = false)}>
                 <div class="navbar-end">
                     {#each layout as parent}
                         {#if parent.children?.length}
@@ -96,8 +91,7 @@
                                     class:is-active={active(parent)}
                                     class:has-text-white={!expanded}
                                     href={parent.route}
-                                    target={parent.external ? "_blank" : ""}
-                                >
+                                    target={parent.external ? "_blank" : ""}>
                                     <b>{parent.label}</b>
                                 </a>
 
@@ -105,8 +99,7 @@
                                     class="navbar-dropdown"
                                     class:is-boxed={!dropdownBoxed}
                                     class:last-dropdown={parent ==
-                                        layout[layout.length - 1]}
-                                >
+                                        layout[layout.length - 1]}>
                                     {#each parent.children as child}
                                         <a
                                             class="navbar-item"
@@ -116,8 +109,7 @@
                                             href={child.route}
                                             target={child.external
                                                 ? "_blank"
-                                                : ""}
-                                        >
+                                                : ""}>
                                             {child.label}
                                         </a>
                                     {/each}
@@ -128,8 +120,7 @@
                                 class="navbar-item"
                                 class:is-active={active(parent)}
                                 class:has-text-white={!expanded}
-                                href={parent.route}
-                            >
+                                href={parent.route}>
                                 <b>{parent.label}</b>
                             </a>
                         {/if}
