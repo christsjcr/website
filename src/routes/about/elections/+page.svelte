c7198473afc52b6b775cd113200f6f305cb31772
Miles Watson <contact@mileswatson.net>
2022-11-19 20:40:15 +0000

    Updated elections page.

--- src/routes/about/elections/+page.svelte
+++ src/routes/about/elections/+page.svelte
@@ -30,8 +30,162 @@
             in week 7. Below is the timetable and list of roles for each of the
             two voting rounds.
         </p>
+        <blockquote>
+            The only role that did not receive any candidates for it was Male &
+            Non-Binary First Year Officer. As a result, a small by-election will
+            be held soon to decide who will take on this role. Any first year
+            may apply for the role as it will become just another First Year
+            Officer role (i.e. there will be no restriction on the gender of
+            applicants). More information will be emailed out in a couple of
+            weeks.
+        </blockquote>
         <br />
-        <TitleBox header="Week 6 (1st half)">
+        <TitleBox header="Week 7 (2nd half)">
+            <h5>Timeline</h5>
+            <ul>
+                <li>Wed 16th Nov 11am - Nominations open (Online)</li>
+                <li>Sat 19th Nov 11am - Nominations close</li>
+                <li>Sun 20th Nov 5-6pm - Hustings #2 (Lloyd Room)</li>
+                <li>Tue 22nd Nov 1pm - Elections open (Online)</li>
+                <li>Wed 23rd Nov 1pm - Elections close</li>
+                <li>Wed 23rd Nov [afternoon] - Results sent out</li>
+            </ul>
+            <h5>Roles up for election</h5>
+            <ul>
+                <li>Vice President</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1kHFye2evNcVPKl7ZpKSjJyMNErKGip5E/view?usp=share_link">
+                            Benjamin Knight
+                        </ExternalLink>
+                    </li>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1OQMePHJuHVCkJ61C9mNeXt0tm0Jjpw5u/view?usp=share_link">
+                            Christina Lawrence
+                        </ExternalLink>
+                    </li>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1PoAUcA6ueh1qHtUKJUgkmWvuAB8seMuF/view?usp=share_link">
+                            John Thompson
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Treasurer</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/14FEcyg4DTPu-QJEAr8fj1ZFyhe_DOkRR/view?usp=share_link">
+                            Fergus Kirman
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Secretary</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1BPZbEMOF5yKz1WglElRQhztP9lpJNQ8l/view?usp=share_link">
+                            Chiara Delpiano Cordeiro
+                        </ExternalLink>
+                    </li>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1uGdhfI6q8VwI7Sa43qlZrP-QkfEwMZCB/view?usp=share_link">
+                            Georgie Van Dyke
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Female and Non-Binary Freshers' Rep</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/14L3t7NVi8I2RHpvAgdZc8ajYXG3Nbz58/view?usp=share_link">
+                            Eleanor Brettell
+                        </ExternalLink>
+                    </li>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1B-uK8L3p1rCU2rCia756gKlXR9rergkX/view?usp=share_link">
+                            Isabelle Mohammed
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>LGBT+ Officer</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1tytYxW9dhg0mM0fu9iv77biopiZvNJmn/view?usp=share_link">
+                            Kas Bridges
+                        </ExternalLink>
+                    </li>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1Koi14Ma7US_KmPeS2LTR53wec-7uH5lS/view?usp=share_link">
+                            Vivek Bilous
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Class Act Officer</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1Fx0_akOF-jsu9RTjsUrg2RswH_8Mxjxk/view?usp=share_link">
+                            Kai Briggs
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Women's Officer</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1dfg1DbdgOoRrL-sySICMiJwIq2cBK4Wf/view?usp=share_link">
+                            Ana-Maria Skaricic
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Internationals' Officer</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1M8EqjJtqUhC5YUnn8TY2Oh1S7YJzTFbJ/view?usp=share_link">
+                            Giannis Karageorgiou
+                        </ExternalLink>
+                    </li>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1hLgHiD3Z7Xj7HMK0RpFIefYA0WhQL3ol/view?usp=share_link">
+                            Mariam Alireza
+                        </ExternalLink>
+                    </li>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1DD5T4IF5VxF0DzycGNL4gpamZIopN28N/view?usp=share_link">
+                            Ju Yi Tai
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Catering and Facilities Officer</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/146ZFHEDMNjqZSqThfMLvYkXY1ORNYrK1/view?usp=share_link">
+                            Mikolaj Rycek
+                        </ExternalLink>
+                    </li>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/18CUadV7r9QzO5wxlg_fqdT0nDFxfpyNY/view?usp=share_link">
+                            Skyler Roberts
+                        </ExternalLink>
+                    </li>
+                </ul>
+            </ul>
+        </TitleBox>
+        <br />
+        <br />
+        <TitleBox header="Week 6 (1st half, finished)" color="dark">
             <h5>Timeline</h5>
             <ul>
                 <li>Tue 8th Nov 11am - Nominations open (Online)</li>
@@ -41,7 +195,7 @@
                 <li>Tue 15th Nov 1pm - Elections close</li>
                 <li>Tue 15th Nov [afternoon] - Results sent out</li>
             </ul>
-            <h5>Roles up for election</h5>
+            <h5>Elected Roles</h5>
             <ul>
                 <li>President</li>
                 <ul>
@@ -63,12 +217,6 @@
                 </ul>
                 <li>Male and Non-Binary Welfare Officer</li>
                 <ul>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/1Gk-FArMji-g2RicjjAZz1wlk35s5aZE9/view?usp=share_link">
-                            Ben Cartwright
-                        </ExternalLink>
-                    </li>
                     <li>
                         <ExternalLink
                             href="https://drive.google.com/file/d/1XgXeATxz7hXvS8Z4tGyErFQh-hz6BNZS/view?usp=share_link">
@@ -120,39 +268,7 @@
                             Juno Buchan
                         </ExternalLink>
                     </li>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/1c-CHxZidWy49BCCNbI54LvZWNiswEH6L/view?usp=share_link">
-                            Emily Freeman
-                        </ExternalLink>
-                    </li>
                 </ul>
-            </ul>
-        </TitleBox>
-        <br />
-        <br />
-        <TitleBox header="Week 7 (2nd half)">
-            <h5>Timeline</h5>
-            <ul>
-                <li>Wed 16th Nov 11am - Nominations open (Online)</li>
-                <li>Sat 19th Nov 11am - Nominations close</li>
-                <li>Sun 20th Nov 5-6pm - Hustings #2 (Lloyd Room)</li>
-                <li>Tue 22nd Nov 1pm - Elections open (Online)</li>
-                <li>Wed 23rd Nov 1pm - Elections close</li>
-                <li>Wed 23rd Nov [afternoon] - Results sent out</li>
-            </ul>
-            <h5>Roles up for election</h5>
-            <ul>
-                <li>Vice President</li>
-                <li>Treasurer</li>
-                <li>Secretary</li>
-                <li>First Year Officer</li>
-                <li>LGBT+ Officer</li>
-                <li>Class Act Officer</li>
-                <li>Women's Officer</li>
-                <li>Internationals' Officer</li>
-                <li>Catering and Facilities Officer</li>
-                <li>... plus any unfilled positions from the 1st half</li>
             </ul>
         </TitleBox>
     </Content>
