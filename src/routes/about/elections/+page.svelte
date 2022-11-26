91f95e9a215953ceff5d6a91012db07d0878fafd
Miles Watson <contact@mileswatson.net>
2022-11-26 14:46:50 +0000

    Updated elected roles.

--- src/routes/about/elections/+page.svelte
+++ src/routes/about/elections/+page.svelte
@@ -40,136 +40,7 @@
             weeks.
         </blockquote>
         <br />
-        <TitleBox header="Week 7 (2nd half)">
-            <h5>Timeline</h5>
-            <ul>
-                <li>Wed 16th Nov 11am - Nominations open (Online)</li>
-                <li>Sat 19th Nov 11am - Nominations close</li>
-                <li>Sun 20th Nov 5-6pm - Hustings #2 (Lloyd Room)</li>
-                <li>Tue 22nd Nov 1pm - Elections open (Online)</li>
-                <li>Wed 23rd Nov 1pm - Elections close</li>
-                <li>Wed 23rd Nov [afternoon] - Results sent out</li>
-            </ul>
-            <h5>Roles up for election</h5>
-            <ul>
-                <li>Vice President</li>
-                <ul>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/1kHFye2evNcVPKl7ZpKSjJyMNErKGip5E/view?usp=share_link">
-                            Benjamin Knight
-                        </ExternalLink>
-                    </li>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/1OQMePHJuHVCkJ61C9mNeXt0tm0Jjpw5u/view?usp=share_link">
-                            Christina Lawrence
-                        </ExternalLink>
-                    </li>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/1PoAUcA6ueh1qHtUKJUgkmWvuAB8seMuF/view?usp=share_link">
-                            John Thompson
-                        </ExternalLink>
-                    </li>
-                </ul>
-                <li>Treasurer</li>
-                <ul>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/14FEcyg4DTPu-QJEAr8fj1ZFyhe_DOkRR/view?usp=share_link">
-                            Fergus Kirman
-                        </ExternalLink>
-                    </li>
-                </ul>
-                <li>Secretary</li>
-                <ul>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/1BPZbEMOF5yKz1WglElRQhztP9lpJNQ8l/view?usp=share_link">
-                            Chiara Delpiano Cordeiro
-                        </ExternalLink>
-                    </li>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/1uGdhfI6q8VwI7Sa43qlZrP-QkfEwMZCB/view?usp=share_link">
-                            Georgie Van Dyke
-                        </ExternalLink>
-                    </li>
-                </ul>
-                <li>Female and Non-Binary Freshers' Rep</li>
-                <ul>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/14L3t7NVi8I2RHpvAgdZc8ajYXG3Nbz58/view?usp=share_link">
-                            Eleanor Brettell
-                        </ExternalLink>
-                    </li>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/1B-uK8L3p1rCU2rCia756gKlXR9rergkX/view?usp=share_link">
-                            Isabelle Mohammed
-                        </ExternalLink>
-                    </li>
-                </ul>
-                <li>Class Act Officer</li>
-                <ul>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/1Fx0_akOF-jsu9RTjsUrg2RswH_8Mxjxk/view?usp=share_link">
-                            Kai Briggs
-                        </ExternalLink>
-                    </li>
-                </ul>
-                <li>Women's Officer</li>
-                <ul>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/1dfg1DbdgOoRrL-sySICMiJwIq2cBK4Wf/view?usp=share_link">
-                            Ana-Maria Skaricic
-                        </ExternalLink>
-                    </li>
-                </ul>
-                <li>Internationals' Officer</li>
-                <ul>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/1M8EqjJtqUhC5YUnn8TY2Oh1S7YJzTFbJ/view?usp=share_link">
-                            Giannis Karageorgiou
-                        </ExternalLink>
-                    </li>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/1hLgHiD3Z7Xj7HMK0RpFIefYA0WhQL3ol/view?usp=share_link">
-                            Mariam Alireza
-                        </ExternalLink>
-                    </li>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/1DD5T4IF5VxF0DzycGNL4gpamZIopN28N/view?usp=share_link">
-                            Ju Yi Tai
-                        </ExternalLink>
-                    </li>
-                </ul>
-                <li>Catering and Facilities Officer</li>
-                <ul>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/146ZFHEDMNjqZSqThfMLvYkXY1ORNYrK1/view?usp=share_link">
-                            Mikolaj Rycek
-                        </ExternalLink>
-                    </li>
-                    <li>
-                        <ExternalLink
-                            href="https://drive.google.com/file/d/18CUadV7r9QzO5wxlg_fqdT0nDFxfpyNY/view?usp=share_link">
-                            Skyler Roberts
-                        </ExternalLink>
-                    </li>
-                </ul>
-            </ul>
-        </TitleBox>
-        <br />
-        <br />
+
         <TitleBox header="Week 6 (1st half, finished)" color="dark">
             <h5>Timeline</h5>
             <ul>
@@ -251,6 +122,94 @@
                         <ExternalLink
                             href="https://drive.google.com/file/d/1ea9IGbggcJiyOd5_BK6RlsP6hyqpRI53/view?usp=share_link">
                             Juno Buchan
+                        </ExternalLink>
+                    </li>
+                </ul>
+            </ul>
+        </TitleBox>
+        <br />
+        <br />
+        <TitleBox header="Week 7 (2nd half, finished)" color="dark">
+            <h5>Timeline</h5>
+            <ul>
+                <li>Wed 16th Nov 11am - Nominations open (Online)</li>
+                <li>Sat 19th Nov 11am - Nominations close</li>
+                <li>Sun 20th Nov 5-6pm - Hustings #2 (Lloyd Room)</li>
+                <li>Tue 22nd Nov 1pm - Elections open (Online)</li>
+                <li>Wed 23rd Nov 1pm - Elections close</li>
+                <li>Wed 23rd Nov [afternoon] - Results sent out</li>
+            </ul>
+            <h5>Elected Roles</h5>
+            <ul>
+                <li>Vice President</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1OQMePHJuHVCkJ61C9mNeXt0tm0Jjpw5u/view?usp=share_link">
+                            Christina Lawrence
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Treasurer</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/14FEcyg4DTPu-QJEAr8fj1ZFyhe_DOkRR/view?usp=share_link">
+                            Fergus Kirman
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Secretary</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1BPZbEMOF5yKz1WglElRQhztP9lpJNQ8l/view?usp=share_link">
+                            Chiara Delpiano Cordeiro
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Female and Non-Binary Freshers' Rep</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1B-uK8L3p1rCU2rCia756gKlXR9rergkX/view?usp=share_link">
+                            Isabelle Mohammed
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Class Act Officer</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1Fx0_akOF-jsu9RTjsUrg2RswH_8Mxjxk/view?usp=share_link">
+                            Kai Briggs
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Women's Officer</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1dfg1DbdgOoRrL-sySICMiJwIq2cBK4Wf/view?usp=share_link">
+                            Ana-Maria Skaricic
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Internationals' Officer</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/1DD5T4IF5VxF0DzycGNL4gpamZIopN28N/view?usp=share_link">
+                            Ju Yi Tai
+                        </ExternalLink>
+                    </li>
+                </ul>
+                <li>Catering and Facilities Officer</li>
+                <ul>
+                    <li>
+                        <ExternalLink
+                            href="https://drive.google.com/file/d/146ZFHEDMNjqZSqThfMLvYkXY1ORNYrK1/view?usp=share_link">
+                            Mikolaj Rycek
                         </ExternalLink>
                     </li>
                 </ul>
