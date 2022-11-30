57a9136ff5a0cd0f3d217262b6ad5dde68ed1678
Miles Watson <contact@mileswatson.net>
2022-11-30 18:59:37 +0000

    Added core feedback form components.

---
+++ src/routes/get-involved/feedback/success/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import Content from "$components/elements/Content.svelte";
+    import PageHeader from "$components/PageHeader.svelte";
+</script>
+
+<PageHeader
+    current="/get-involved/feedback/success"
+    title="Feedback"
+    size="small"
+    metaDescription="">
+    <Content>
+        <section class="hero is-info">
+            <div class="hero-body">
+                <p class="title">Feedback Submitted Successfully!</p>
+                <p class="subtitle">
+                    Your feedback has been submitted successfully. We will do
+                    our best to get back to you in a reasonable timeframe.
+                </p>
+                <p class="subtitle">
+                    <u><a class="link" href="/">Go Home →</a></u>
+                </p>
+            </div>
+        </section>
+    </Content>
+</PageHeader>
