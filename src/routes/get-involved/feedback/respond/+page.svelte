8a4455e75ba7356a079ac0b5e5952f1c7b266efc
Miles Watson <contact@mileswatson.net>
2022-12-05 18:54:21 +0000

    Added response form.

---
+++ src/routes/get-involved/feedback/respond/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import Content from "$components/elements/Content.svelte";
+    import PageHeader from "$components/PageHeader.svelte";
+    import { onMount } from "svelte";
+    import ResponseForm from "./ResponseForm.svelte";
+
+    let recipient: string;
+    let subject: string;
+
+    onMount(() => {
+        const params = new URLSearchParams(document.location.search);
+        recipient = params.get("recipient");
+        subject = params.get("subject");
+    });
+</script>
+
+<PageHeader
+    current="/get-involved/feedback/respond"
+    title="Feedback Response"
+    size="small"
+    metaDescription="">
+    <Content>
+        {#if recipient && subject}
+            <ResponseForm {subject} {recipient} />
+        {/if}
+    </Content>
+</PageHeader>
---
+++ src/routes/get-involved/feedback/respond/ResponseForm.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+    export let subject: string;
+    export let recipient: string;
+    let message: string = "";
+
+    let agreed: boolean = false;
+</script>
+
+<form action="https://members.thejcr.co.uk/api/feedback/respond" method="POST">
+    <input type="hidden" name="recipient" value={recipient} />
+    <div class="field">
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label class="label">Subject</label>
+        <div class="control">
+            <input
+                value={subject}
+                name="subject"
+                class="input"
+                type="text"
+                readonly />
+        </div>
+    </div>
+    <div class="field">
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label class="label">Message</label>
+        <div class="control">
+            <textarea
+                bind:value={message}
+                name="message"
+                class="textarea"
+                placeholder="e.g. Thanks for letting us know about..." />
+        </div>
+    </div>
+    <div class="field">
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label class="label">Terms and Conditions</label>
+        <div class="control">
+            <label class="checkbox">
+                <input name="agreed" type="checkbox" bind:checked={agreed} />
+                I have discussed this response with all relevant recipients of the
+                original feedback, as well as members of the exec if the subject
+                is sensitive.
+            </label>
+        </div>
+    </div>
+    <div class="field">
+        <div class="control">
+            <button
+                class="button is-link"
+                disabled={!agreed || message.length == 0}
+                >Login & Submit</button>
+        </div>
+    </div>
+</form>
