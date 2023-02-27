d4f63654bc84d40d70c2fddfb02a1d6e123cd818
Miles Watson <contact@mileswatson.net>
2023-02-27 11:10:51 +0000

    Added hall timings.

--- src/components/features/Meal.svelte
+++ src/components/features/Meal.svelte
@@ -5,11 +5,33 @@
 <script lang="ts">
     import TitleBox from "$components/elements/TitleBox.svelte";
 
-    export let header: string;
+    export let type: "lunch" | "dinner";
     export let data: Meal = null;
+    export let dayOfWeek: number; // 0 for Sunday, 1 for Monday, etc
+
+    $: isWeekend = dayOfWeek == 0 || dayOfWeek == 6;
+
+    $: header = {
+        lunch: isWeekend ? "🍳 Brunch" : "🥪 Lunch",
+        dinner: "🍕 Dinner",
+    }[type];
+
+    $: getMealTime = () => {
+        if (type === "lunch") {
+            if (dayOfWeek == 6) return "11:00-12:30";
+            if (dayOfWeek == 0) return "10:30-12:00";
+            return "12:00-13:45";
+        } else {
+            if (isWeekend) return "17:50-19:00";
+            return "17:50-19:20";
+        }
+    };
 </script>
 
-<TitleBox {header}>
+<TitleBox
+    {header}
+    alongside={getMealTime()}
+    infoLink={"http://localhost:5173/resources/college-life#Meals"}>
     <div class="content">
         {#if data}
             <h5>Mains</h5>
