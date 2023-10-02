83adebfc68f5519d88833012b6397a8024e482d5
Miles Watson <contact@mileswatson.net>
2023-10-02 20:24:39 +0100

    Updated termcard.

--- scripts/calendar.ts
+++ scripts/calendar.ts
@@ -1,6 +1,7 @@
 import { getICS } from "../src/components/events/event";
 import { year, freshers } from "../src/routes/get-involved/freshers/freshers";
 import { meetings } from "../src/routes/about/meetings/meetings";
+import { termcard } from "../src/routes/get-involved/termcard/termcard";
 import fs from "fs";
 
 const compulsory = getICS(`Compulsory Freshers Events ${year}`, freshers.filter(x => x.type === "compulsory"));
@@ -18,3 +19,10 @@ fs.mkdirSync("./static/calendar/meetings", {recursive: true});
 fs.writeFileSync(`./static/calendar/meetings/open.ics`, open);
 fs.writeFileSync(`./static/calendar/meetings/council.ics`, council);
 fs.writeFileSync(`./static/calendar/meetings/exec.ics`, exec);
+
+const jcr = getICS("JCR Termcard", termcard.filter(x => x.type === "jcr"));
+const society = getICS("Society Termcard", termcard.filter(x => x.type === "society"));
+
+fs.mkdirSync("./static/calendar/termcard", {recursive: true});
+fs.writeFileSync(`./static/calendar/termcard/jcr.ics`, jcr);
+fs.writeFileSync(`./static/calendar/termcard/society.ics`, society);
---
+++ src/routes/get-involved/termcard/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import Content from "$components/elements/Content.svelte";
+    import Calendar from "$components/events/Calendar.svelte";
+    import NotificationLink from "$components/elements/NotificationLink.svelte";
+    import PageHeader from "$components/PageHeader.svelte";
+    import { termcard, term } from "./termcard";
+</script>
+
+<PageHeader
+    current="/get-involved/termcard"
+    title="Freshers"
+    size="small"
+    metaDescription="See what's coming up this term on the official JCR termcard!">
+    <Content hasText={false}>
+        <div class="columns">
+            <div class="column is-narrow">
+                <a href={`webcal://thejcr.co.uk/calendar/termcard/jcr.ics`}>
+                    <button class="button is-success"
+                        >Subscribe to JCR Events</button>
+                </a>
+            </div>
+            <div class="column is-narrow">
+                <a href={`webcal://thejcr.co.uk/calendar/termcard/society.ics`}>
+                    <button class="button is-primary"
+                        >Subscribe to Society Events</button>
+                </a>
+            </div>
+        </div>
+        <Calendar
+            events={termcard}
+            colors={{ society: "#A28000", jcr: "#146A46" }}
+            type="listYear" />
+    </Content>
+</PageHeader>
