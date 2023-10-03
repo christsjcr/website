bfc13e93f6d408dbe45e1a2f41e46f5ba41c70af
Miles Watson <contact@mileswatson.net>
2023-10-03 09:43:18 +0100

    Fixed termcard formatting.

--- src/routes/get-involved/termcard/+page.svelte
+++ src/routes/get-involved/termcard/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
     import Content from "$components/elements/Content.svelte";
     import Calendar from "$components/events/Calendar.svelte";
-    import NotificationLink from "$components/elements/NotificationLink.svelte";
     import PageHeader from "$components/PageHeader.svelte";
     import { termcard, term } from "./termcard";
 </script>
 
 <PageHeader
     current="/get-involved/termcard"
-    title="Freshers"
+    title="Termcard"
     size="small"
     metaDescription="See what's coming up this term on the official JCR termcard!">
     <Content hasText={false}>
+        <h2 class="title is-2">{term} Term</h2>
         <div class="columns">
             <div class="column is-narrow">
                 <a href={`webcal://thejcr.co.uk/calendar/termcard/jcr.ics`}>
