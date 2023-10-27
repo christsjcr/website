280b8a4ecac648123de9dd22bc4a26935a1ac832
Miles Watson <contact@mileswatson.net>
2023-10-27 21:56:24 +0100

    Added page for creating a society.

---
+++ src/routes/get-involved/societies/create/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import Content from "$components/elements/Content.svelte";
+    import PageHeader from "$components/PageHeader.svelte";
+</script>
+
+<PageHeader
+    current="/get-involved/societies/create"
+    title="Create a Society"
+    size="small"
+    metaDescription="Learn how to create a new student society at Christ's College, Cambridge.">
+    <Content>
+        <p>
+            Through the JCR, Christ's undergraduates can gain permission to
+            establish a society and secure funding from our annual budget for
+            their events and operations.
+        </p>
+        <h3>Steps</h3>
+        <ol>
+            <li>
+                Write a constitution and/or summary of fundamental aims of your
+                society, and decide who will be in charge of running the society
+                for the foreseeable future. You'll need to propose this to the
+                college Dean. You can find an example of a constitution /
+                summary of fundamental aims <a href="">here</a>.
+            </li>
+            <li>
+                Think about your society's events for the rest of the calendar
+                year and estimate the potential costs, including things such as
+                refreshments, speaker travel expenses, and sports equipment.
+                Detail these expenses in a budget application, which should then
+                be discussed with the JCR Treasurer. An example of a budget
+                application can be found <a
+                    href="https://docs.google.com/spreadsheets/d/1FNIpraJXga2hKfv_pXffPIViTKszFiPz/edit?usp=drive_link&ouid=112194519812852594064&rtpof=true&sd=true"
+                    >here</a
+                >.
+            </li>
+            <li>
+                Check the <a href="/about/meetings">JCR Meeting Calendar</a> to find
+                the next Open Meeting. Submit your budget application and constitution
+                / summary of fundamental aims to either the JCR Secretary or JCR
+                President to be discussed at the meeting. The proposal will be voted
+                upon, and if approved, your society will be officially established.
+            </li>
+        </ol>
+        <p>
+            For further assistance or enquiries, contact a <a
+                href="/about/committee">Committee member</a>
+            or use the
+            <a href="/get-involved/feedback">Anonymous Feedback Form</a>.
+        </p>
+    </Content>
+</PageHeader>
