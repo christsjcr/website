9ee7c59353fc98c5cb9ac51830c9c96e0bf99fb2
Miles Watson <contact@mileswatson.net>
2023-10-30 12:56:41 +0000

    Updated examples link.

--- src/routes/get-involved/societies/create/+page.svelte
+++ src/routes/get-involved/societies/create/+page.svelte
@@ -20,19 +20,14 @@
                 Write a constitution and/or summary of fundamental aims of your
                 society, and decide who will be in charge of running the society
                 for the foreseeable future. You'll need to propose this to the
-                college Dean. You can find an example of a constitution /
-                summary of fundamental aims <a href="">here</a>.
+                college Dean.
             </li>
             <li>
                 Think about your society's events for the rest of the calendar
                 year and estimate the potential costs, including things such as
                 refreshments, speaker travel expenses, and sports equipment.
                 Detail these expenses in a budget application, which should then
-                be discussed with the JCR Treasurer. An example of a budget
-                application can be found <a
-                    href="https://docs.google.com/spreadsheets/d/1FNIpraJXga2hKfv_pXffPIViTKszFiPz/edit?usp=drive_link&ouid=112194519812852594064&rtpof=true&sd=true"
-                    >here</a
-                >.
+                be discussed with the JCR Treasurer.
             </li>
             <li>
                 Check the <a href="/about/meetings">JCR Meeting Calendar</a> to find
@@ -42,6 +37,13 @@
                 upon, and if approved, your society will be officially established.
             </li>
         </ol>
+        <p>
+            You can find examples of budget applications and constitutions /
+            summaries of fundamental aims <a
+                href="https://drive.google.com/drive/folders/1MYJG5I-zHsFfrxxlODiKICNDi0dsyqtI?usp=drive_link"
+                >here</a
+            >.
+        </p>
         <p>
             For further assistance or enquiries, contact a <a
                 href="/about/committee">Committee member</a>
