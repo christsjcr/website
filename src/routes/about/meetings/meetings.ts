fad1b51e64d22e74d8217303db2f60f84037c2dc
rachleona <ractam2002@gmail.com>
2024-01-18 18:18:30 +0000

    Updated meeting schedule and termcard

--- src/routes/about/meetings/meetings.ts
+++ src/routes/about/meetings/meetings.ts
@@ -1,29 +1,21 @@
 import type { Events } from "$components/events/event";
 
-const year: number = 2023;
+const year: number = 2024;
 
-export const term: string = "Michaelmas";
+export const term: string = "Lent";
 
 export const meetings: Events<"exec" | "council" | "open"> = [
     {
-        description: "JCR Council Meeting",
-        date: [year, 10, 7],
+        description: "JCR Exec Meeting",
+        date: [year, 1, 20],
         time: [17, 0],
         duration: [1, 0],
         location: "Lloyd Room",
-        type: "council",
-    },
-    {
-        description: "JCR Exec Meeting",
-        date: [year, 10, 15],
-        time: [15, 0],
-        duration: [1, 0],
-        location: "N5",
         type: "exec",
     },
     {
         description: "JCR Council Meeting",
-        date: [year, 10, 21],
+        date: [year, 1, 27],
         time: [17, 0],
         duration: [1, 0],
         location: "Lloyd Room",
@@ -31,15 +23,15 @@ export const meetings: Events<"exec" | "council" | "open"> = [
     },
     {
         description: "JCR Exec Meeting",
-        date: [year, 10, 28],
-        time: [14, 30],
+        date: [year, 2, 3],
+        time: [14, 0],
         duration: [1, 0],
-        location: "Lloyd Room",
+        location: "Sosnow Room",
         type: "exec",
     },
     {
         description: "JCR Open Meeting",
-        date: [year, 10, 28],
+        date: [year, 2, 3],
         time: [17, 0],
         duration: [1, 0],
         location: "Lloyd Room",
@@ -47,7 +39,7 @@ export const meetings: Events<"exec" | "council" | "open"> = [
     },
     {
         description: "JCR Council Meeting",
-        date: [year, 11, 4],
+        date: [year, 2, 10],
         time: [17, 0],
         duration: [1, 0],
         location: "Lloyd Room",
@@ -55,15 +47,15 @@ export const meetings: Events<"exec" | "council" | "open"> = [
     },
     {
         description: "JCR Exec Meeting",
-        date: [year, 11, 11],
+        date: [year, 2, 17],
         time: [17, 0],
         duration: [1, 0],
-        location: "Lloyd Room",
+        location: "NCSR",
         type: "exec",
     },
     {
         description: "JCR Council Meeting",
-        date: [year, 11, 18],
+        date: [year, 2, 24],
         time: [17, 0],
         duration: [1, 0],
         location: "Lloyd Room",
@@ -71,18 +63,26 @@ export const meetings: Events<"exec" | "council" | "open"> = [
     },
     {
         description: "JCR Exec Meeting",
-        date: [year, 11, 25],
-        time: [14, 30],
+        date: [year, 3, 2],
+        time: [14, 0],
         duration: [1, 0],
-        location: "Lloyd Room",
+        location: "Sosnow Room",
         type: "exec",
     },
     {
         description: "JCR Open Meeting",
-        date: [year, 11, 25],
+        date: [year, 3, 9],
         time: [17, 0],
         duration: [1, 0],
         location: "Lloyd Room",
         type: "open",
+    },
+    {
+        description: "JCR Council Meeting",
+        date: [year, 3, 10],
+        time: [17, 0],
+        duration: [1, 0],
+        location: "TBC",
+        type: "council",
     },
 ];
